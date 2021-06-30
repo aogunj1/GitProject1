a71ddbf95764fe4cec2587e32e7faed4f970ecf5
aogunj1 <86618768+aogunj1@users.noreply.github.com>
2021-06-29 22:15:48 -0400

    DieRoll

--- gitProject1/src/gitProject1/dieRoll.java
+++ gitProject1/src/gitProject1/dieRoll.java
@@ -1,9 +1,33 @@
 package gitProject1;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class dieRoll {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//Six sided die rolling game. Die rolls and keeps track od how many tries it takes you to guess correctly.System.out.println("Pick a number between 0-5");
+		
+				Scanner scanner = new Scanner(System.in);
+				Random random = new Random();
+				 int randNum = 0;
+				 int myNum = 0;
+				 int tryCounter = 0;
+				
+				do 
+				{
+				randNum = random.nextInt(6);
+				myNum = scanner.nextInt();
+				System.out.println("it rolled " + randNum);
+				tryCounter++;
+				}
+				while(randNum != myNum);
+				
+				System.out.println("You got it ");
+				System.out.println("You guessed " + myNum);
+				System.out.println("Die rolled " + randNum);
+				System.out.println("it took you " + tryCounter + " trie
 
 	}
 
